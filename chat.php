203756a3c98a72514856a7f95fbdd027aa0d8066
Sasmita-sys <79735970+Sasmita-sys@users.noreply.github.com>
2021-03-24 11:15:18 +0700

    Update chat.php

--- chat.php
+++ chat.php
@@ -1,46 +1,4 @@
-<!-- <!DOCTYPE html>
-<html>
-<head>
-	<title>Chat</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-	<h1 id="title">Welcome</h1>
-	<p id="desciption">Keep in mind to be polite with each other!.</p>
-	<form action="chat.php" method="post">
-        <label for="kolom">
-            <?php
-                $myfile = fopen("chat.txt", "r") or die("Unable to open the file!");
-                while(!feof($myfile)) {
-                    echo fgets($myfile) . "<br>";
-                }
-                header("chat.php");
-            ?>
-        </label>
-        <label for="cont">
-            Message:<br>
-            <input name="cont" type="text" />
-		</label>
-        <label for="send">
-            <input type="submit" name="send" value="Send" />
-        </label>
-	</form>
-</body>
-</html> -->
 <?php
-// if (isset($_POST['send'])){
-//     $chat = $_POST['cont'];
-//     $file = file_get_contents("chat.txt");
-//     $string = "$chat";
-//     if(!strstr($file, "$string")){
-//         $myFile = "chat.txt";
-//         $fh = fopen($myFile, 'a') or die("Unable to open the file");
-//         $stringData = "$chat\n";
-//         fwrite($fh, $stringData);
-//         fclose($fh);
-//         header("chat.php");
-//     }
-// }
 session_start();
 
 if(isset($_POST['ajaxsend']) && $_POST['ajaxsend']==true){
